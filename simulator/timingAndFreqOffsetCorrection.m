e06c942ea5c22d1ba074b23d31f098efe46b5af5
Michael Heiniger <michael.heiniger@epfl.ch>
2017-05-05 11:20:31 +0200

    Variable name refactoring

--- simulator/timingAndFreqOffsetCorrection.m
+++ simulator/timingAndFreqOffsetCorrection.m
@@ -1,26 +1,24 @@
-function [frameCorrected, frequencyOffsetEst] = timingAndFreqOffsetCorrection(signal, shortTrainingSeqIfft, longTrainingSeqIfft, numTotalCarriers, CPLength, preamble2CPLength, numOFDMSymbolsPerFrame)
+function [frameCorrected, frequencyOffsetEst] = timingAndFreqOffsetCorrection(signal, stsTime, ltsTime, numTotalCarriers, CPLength, twoLtsCpLength, numOFDMSymbolsPerFrame)
 %TIMINGANDFREQOFFSET Summary of this function goes here
 %   Detailed explanation goes here
 
-frequencyOffsetEst = 0;
-
-% receivedSignal = signal;
-
 global decoder;
+
+frequencyOffsetEst = 0;
 
 Fs = 0.5e6;
 Ts = 1/Fs;
 
-lengthSTSIfft = length(shortTrainingSeqIfft);
-lengthLTSIfft = length(longTrainingSeqIfft);
+lengthStsTime = length(stsTime);
+lengthLtsTime = length(ltsTime);
 
 if decoder.upsample
     signal = resample(signal, decoder.USF, 1);
     % Timing estimation - find the begining of the next received OFDM frame
-    fineTau = timingEstimation(signal, resample(shortTrainingSeqIfft, decoder.USF, 1));
+    fineTau = timingSync(signal, resample(stsTime, decoder.USF, 1), resample(ltsTime, decoder.USF, 1), twoLtsCpLength);
 else
     % Timing estimation - find the begining of the next received OFDM frame
-    fineTau = timingEstimation(signal, shortTrainingSeqIfft);    
+    fineTau = timingSync(signal, stsTime, ltsTime, twoLtsCpLength);    
 end
 
 % Extract current frame
@@ -29,15 +27,30 @@ signalCut = signal(beginingFrame:end);
 if decoder.upsample
     signalCut = downsample(signalCut, decoder.USF);
 end
-lengthFrame = 10*lengthSTSIfft + preamble2CPLength + 2*lengthLTSIfft + numOFDMSymbolsPerFrame*(CPLength+numTotalCarriers);
+lengthFrame = 10*lengthStsTime + twoLtsCpLength + 2*lengthLtsTime + numOFDMSymbolsPerFrame*(CPLength+numTotalCarriers);
 frame = signalCut(1:lengthFrame);
 
-if decoder.frequencyCorrection
-    [intialPhaseEst, frequencyOffsetEst] = frequencyOffsetEstimation(frame, shortTrainingSeqIfft, longTrainingSeqIfft);
+figure;
+hax=axes;
+plot(abs(signal))
+title('Abs val of received signal');
+VL1 = fineTau;
+line([VL1 VL1],get(hax,'YLim'), 'Color', [0 1 0]);
+VL2 = fineTau+lengthFrame-1;
+line([VL2 VL2],get(hax,'YLim'), 'Color', [0 1 0]);
 
+figure;
+plot(abs(frame));
+title('Received frame');
+
+if decoder.cfoCorrection
+    [intialPhaseEst, frequencyOffsetEst] = frequencyOffsetEstimation(frame, stsTime, ltsTime);
+
+%     frequencyOffsetEst = 237 %TMP: Used to set manually the residual CFO error
+    
     % Correct frequency offset
-    t = (0:length(frame(1+10*lengthSTSIfft+preamble2CPLength:end))-1)*Ts;
-    frameCorrected = [frame(1:10*lengthSTSIfft+preamble2CPLength); frame(1+10*lengthSTSIfft+preamble2CPLength:end) .* exp(-1j*(2*pi*frequencyOffsetEst*t+intialPhaseEst)).'];
+    t = (0:length(frame(1+10*lengthStsTime+twoLtsCpLength:end))-1)*Ts;
+    frameCorrected = [frame(1:10*lengthStsTime+twoLtsCpLength); frame(1+10*lengthStsTime+twoLtsCpLength:end) .* exp(-1j*(2*pi*frequencyOffsetEst*t+intialPhaseEst)).'];
 else
     frameCorrected = frame;
 end
@@ -54,8 +67,7 @@ if decoder.upsample
 end
 
 figure;
-% hax=axes;
-plot(abs(frameCorrected)); %/sum(abs(normalization(1:length(R))).^2));
-title('Corrected received signal');
+plot(abs(frameCorrected));
+title('Corrected received frame');
 
 end
