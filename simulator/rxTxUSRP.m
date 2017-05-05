1783ddc55f2c805a8a03e5f91d17836a7c198ffc
Michael Heiniger <michael.heiniger@epfl.ch>
2017-05-05 10:11:00 +0200

    Move signal padding function call inside rxTxUSRP, move samplesPerFrame value definition inside rxTxUSRP

--- simulator/rxTxUSRP.m
+++ simulator/rxTxUSRP.m
@@ -1,26 +1,9 @@
-function [dataRx,Fs] = rxTxUSRP(dataTx, samplesPerFrame, mode)
+function [signalRx,Fs] = rxTxUSRP(signalTx, mode)
 %RXTXUSRP Summary of this function goes here
 %   Detailed explanation goes here
 
 serialNumberRx = '30C51BC';
 serialNumberTx = '30C5426';
-
-devicesToCheck = [];
-if strcmp(mode, 'loopback')
-    serialNumberRx = serialNumberTx;
-    if samplesPerFrame > 1e5
-        warning('samplePerFrame > 1e5 leads to overrun: the board drops samples.')
-    end
-    devicesToCheck = [serialNumberRx];
-elseif strcmp(mode, 'oneBoardTx')
-    devicesToCheck = [serialNumberTx];
-elseif strcmp(mode, 'oneBoardRx')
-    devicesToCheck = [serialNumberRx];
-elseif strcmp(mode, 'twoBoardsRxTx')
-    devicesToCheck = [serialNumberRx; serialNumberTx];
-end
-
-checkDevicesConnection(devicesToCheck);
 
 boardPlatform = 'B200';
 fc = 2.4e9; % carrier frequency
@@ -31,19 +14,44 @@ clockRateRx = clockRateTx; % on the same radio we need to use the same
 decimationRx = interpolationTx; % to get to 1MHz
 clockInputSource = 'Internal';
 outputDataTypeUSRP = 'double'; % Difference between transport and output data type ?
-gainTx = 60; %60 loopback 30dB attenuator; % 89 over the air, no attenuator
+gainTx = 89; %60 loopback 30dB attenuator; % 89 over the air, no attenuator
 gainRx = 40;
+
+% Number of samples per USRP frame
+samplesPerFrame = 0.5e5;
 
 burstMode = false;
 
 Ts = interpolationTx/clockRateTx; % [s] sampling time
 Fs = 1/Ts;
 
-noFrames = length(dataTx)/samplesPerFrame;
+% Pad signal with zeros
+signalTx = padWithZeros(signalTx, samplesPerFrame);
+
+% Compute the number of USRP frames needed to transmit the signal
+noFrames = length(signalTx)/samplesPerFrame;
 if mod(noFrames, 1) ~= 0
     error('Number of frames must be integer %s.', num2str(noFrames));
 end
 
+devicesToCheck = [];
+if strcmp(mode, 'loopback')
+    serialNumberRx = serialNumberTx;
+    if samplesPerFrame > 1e5
+        warning('samplePerFrame > 1e5 leads to overrun: the board drops samples.')
+    end
+    devicesToCheck = serialNumberRx;
+elseif strcmp(mode, 'oneBoardTx')
+    devicesToCheck = serialNumberTx;
+elseif strcmp(mode, 'oneBoardRx')
+    devicesToCheck = serialNumberRx;
+elseif strcmp(mode, 'twoBoardsRxTx')
+    devicesToCheck = [serialNumberRx; serialNumberTx];
+end
+
+checkDevicesConnection(devicesToCheck);
+
+% Instantiate transmitter object
 if strcmp(mode, 'loopback') || strcmp(mode, 'oneBoardTx') ||  strcmp(mode, 'twoBoardsRxTx')
     tx = comm.SDRuTransmitter(...
         'Platform', boardPlatform, ...
@@ -57,9 +65,10 @@ if strcmp(mode, 'loopback') || strcmp(mode, 'oneBoardTx') ||  strcmp(mode, 'twoB
         'UnderrunOutputPort', true);
 end
 
+% Instantiate receiver object
 if strcmp(mode, 'loopback') || strcmp(mode, 'oneBoardRx') ||  strcmp(mode, 'twoBoardsRxTx')
     
-    dataRx = zeros(1, noFrames*samplesPerFrame);
+    signalRx = zeros(1, noFrames*samplesPerFrame);
     
     rx = comm.SDRuReceiver(...
         'Platform', boardPlatform, ...
@@ -75,6 +84,7 @@ if strcmp(mode, 'loopback') || strcmp(mode, 'oneBoardRx') ||  strcmp(mode, 'twoB
         'OverrunOutputPort', true);
 end
 
+disp('Transmit and/or Receive ...');
 switch mode
     case {'twoBoardsRxTx','loopback'}
         l = 1;
@@ -82,8 +92,8 @@ switch mode
             [dataRxUSRP, len, ~] = rx();
             
             if len > 0
-                dataRx(1+(l-1)*samplesPerFrame:l*samplesPerFrame) = dataRxUSRP.';
-                tx(dataTx(1+(l-1)*samplesPerFrame:l*samplesPerFrame).');
+                signalRx(1+(l-1)*samplesPerFrame:l*samplesPerFrame) = dataRxUSRP.';
+                tx(signalTx(1+(l-1)*samplesPerFrame:l*samplesPerFrame).');
                 l = l+1;
             end
         end
@@ -94,18 +104,20 @@ switch mode
             [dataRxUSRP, len, ~] = rx();
             
             if len > 0
-                dataRx(1+(l-1)*samplesPerFrame:l*samplesPerFrame) = dataRxUSRP.';
+                signalRx(1+(l-1)*samplesPerFrame:l*samplesPerFrame) = dataRxUSRP.';
                 l = l+1;
             end
         end
     case 'oneBoardTx'
         for l = 1:noFrames
-            tx(dataTx(1+(l-1)*samplesPerFrame:l*samplesPerFrame).');
-            dataRx = [];
+            tx(signalTx(1+(l-1)*samplesPerFrame:l*samplesPerFrame).');
+            signalRx = [];
         end
     otherwise
         error('Unknown mode of operation.')
 end
+
+disp('End of transmission and / or reception');
 
 if strcmp(mode, 'loopback') || strcmp(mode, 'oneBoardTx') ||  strcmp(mode, 'twoBoardsRxTx')
     release(tx);
