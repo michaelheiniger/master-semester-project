6d0b9d2279c8d41bfdb2e47ab9d5bb8289d83091
Michael Heiniger <michael.heiniger@epfl.ch>
2017-05-05 11:18:24 +0200

    Remove name-refactored functions

--- simulator/padLastFrameWithZeros.m
+++
@@ -1,9 +0,0 @@
-function [frame] = padLastFrameWithZeros(frame, samplesPerFrame)
-%PADLASTFRAMEWITHZEROS Summary of this function goes here
-%   Detailed explanation goes here
-
-paddingLength = ceil(length(frame)/samplesPerFrame)*samplesPerFrame-length(frame);
-frame = [frame zeros(1, paddingLength)];
-
-end
-
