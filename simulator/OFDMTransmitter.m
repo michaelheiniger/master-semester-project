ea39adae43323c31c64efde4f85110c4392cb339
Michael Heiniger <michael.heiniger@epfl.ch>
2017-05-05 10:54:25 +0200

    Minor variable name and comments refactoring

--- simulator/OFDMTransmitter.m
+++ simulator/OFDMTransmitter.m
@@ -1,53 +1,56 @@
-function [signalTx, dataFrame] = OFDMTransmitter(config, mode, symbolsTx, shortTrainingSeqIFFT, longTrainingSeqIFFT, pilotOfdmSymbol, pilots1, pilots2)
+function [signalTx, dataFrame] = OFDMTransmitter(config, mode, dataSymbolsTx, stsTime, ltsTime, pilotOfdmSymbol, pilots1, pilots2)
 
 c = config;
 
-% First preamble construction
-preamble1 = repmat(shortTrainingSeqIFFT, 10, 1);
+% Build 10 STS
+tenSts = repmat(stsTime, 10, 1);
 
-% Second preamble construction
-preamble2 = repmat(longTrainingSeqIFFT, 2, 1);
+% Build 2 LTS
+twoLts = repmat(ltsTime, 2, 1);
 
-% Add CP to second preamble
-secondPreambleIFFTCP = [preamble2(end-c.preamble2CPLength+1:end); preamble2];
+% Add cyclic prefix (CP) to the two LTS
+twoLtsWithCp = [twoLts(end-c.twoLtsCpLength+1:end); twoLts];
 
 % OFDM data frame construction
-dataFrame = reshape(symbolsTx, c.numDataCarriers-c.numPilots, c.numOFDMSymbolsPerFrame-1);
+dataFrame = reshape(dataSymbolsTx, c.numDataCarriers, c.numOFDMSymbolsPerFrame-1);
+
+% Add pilot OFDM symbol (for channel estimation)
+dataFrame = [pilotOfdmSymbol, dataFrame];
 
 % Add pilot subcarriers (for post-FFT frequency offset correction due to
 % sampling frequency offset)
-
 dataFrame = [pilots1;...
             dataFrame; ...
             pilots2];
 
-% Add pilot ofdm symbol (for channel estimation)
-dataFrame = [pilotOfdmSymbol, dataFrame];
+% Add guard bands (unsued subcarriers)
+% Outer subcarriers are severly attenuated due to SFO. 
+% Not using them also makes guard bands to avoid frequency leakage
+dataFrame = [zeros(c.numZerosTop, c.numOFDMSymbolsPerFrame); ...
+            dataFrame(1:c.numTotalCarriers/2-c.numZerosTop,:); ...
+            zeros(c.dcSubcarrier, c.numOFDMSymbolsPerFrame); ...
+            dataFrame(c.numTotalCarriers/2-c.numZerosTop+1:end,:); ...
+            zeros(c.numZerosBottom, c.numOFDMSymbolsPerFrame)];
 
-dataFrame = [zeros(c.numZeros, c.numOFDMSymbolsPerFrame); ...
-            dataFrame; ...
-            zeros(c.numZeros, c.numOFDMSymbolsPerFrame)];
-
-% Apply ffshift to account for the negative frequencies since the MATLAB
-% function ifft takes k=0,...,N-1 and not k=-N/2,...,0,...,N/2-1
-% Apply ifft to get the time domain signal
+% IFFT: fftshift() is needed to account for the negative frequencies since 
+% the MATLAB function ifft() takes k=0,...,N-1 and not k=-N/2,...,0,...,N/2-1
 dataFrameIFFT = ifft(fftshift(dataFrame,1), c.NFFT);
-% Add CP to data frame
-dataFrameIFFTCP = [dataFrameIFFT(end-c.CPLength+1:end,:);...
+
+% Add cyclic prefix (CP) to every OFDM symbol
+dataFrameIFFTWithCp = [dataFrameIFFT(end-c.CPLength+1:end,:);...
                   dataFrameIFFT];
 
 % Serialization
-signalTx = [preamble1;...
-           secondPreambleIFFTCP;...
-           dataFrameIFFTCP(:)];
-
+signalTx = [tenSts;...
+           twoLtsWithCp;...
+           dataFrameIFFTWithCp(:)];
+       
+% Add garbage before and after OFDM frame and repeat
 if strcmp(mode, 'simulation')
-    signalTx = repmat([zeros(1000,1); signalTx; zeros(1000,1)],10,1);
+    signalTx = repmat([zeros(1000,1); signalTx; zeros(1000,1)],1,1);
 else
-    signalTx = repmat([zeros(1000,1); signalTx; zeros(1000,1)],100,1);
+    signalTx = repmat([complex(0.001+1i*0.001+zeros(1.5e5,1)); signalTx; complex(0.001+1i*0.001+zeros(1.5e5,1))],10,1);
 end
-
-
 
 end
 
