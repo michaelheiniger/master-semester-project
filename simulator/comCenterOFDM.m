e52b2e21df722fd6d5384cf6e4d3c47b6d2eda1a
Michael Heiniger <michael.heiniger@epfl.ch>
2017-05-06 13:40:32 +0200

    Adapt the OFDM pilot symbol to comply with MMSE channel estimation: the estimation must be done on an OFDM pilot symbol that is composed of symbols such that re(symbol) = im(symbol)

--- simulator/comCenterOFDM.m
+++ simulator/comCenterOFDM.m
@@ -21,51 +21,78 @@ fprintf('Instance started on %s \n\n',datestr(now))
 %                       other receives
 % USRP boards are always used in half-duplex mode
 
+% mode = 'twoBoardsRxTx';
 mode = 'simulation';
-samplesPerFrame = 5e4;
 
 % Decoder parameters
 decoderConfig()
 
+config.Fs = 0.5e6; % Hz
 config.M = 4;
 config.map = qammap(config.M);
-config.numTotalCarriers = 128;
-config.numZeros = 13;
+config.numTotalCarriers = 64;
+marginSubCarriers = 0.17*config.numTotalCarriers;
+% config.numZerosTop = 0;
+% config.numZerosBottom = 0;
+config.numZerosTop = ceil(marginSubCarriers/2);
+config.numZerosBottom = floor(marginSubCarriers/2);
+config.dcSubcarrier = 1; % 0 if used for data, 1 means that it isn't used to send data
 config.numPilots = 2;
 config.NFFT = config.numTotalCarriers;
 config.NFFT_TS = 64;
 config.CPLength = 16;
-config.preamble2CPLength = 32;
-config.numOFDMSymbolsPerFrame = 30; % Excluding preamble
-config.numDataCarriers = config.numTotalCarriers - 2*config.numZeros;
+config.twoLtsCpLength = 32;
+config.numOFDMSymbolsPerFrame = 100; % Excluding preamble, must be >= 2
+config.numUsedCarriers = config.numTotalCarriers - config.numZerosTop - config.numZerosBottom - config.dcSubcarrier;
+config.numDataCarriers = config.numUsedCarriers - config.numPilots;
+
+% Display config
+config 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Preambles
+% Training sequence for preamble
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-load('shortTrainingSeq.mat');
-load('longTrainingSeq.mat');
+% Short Training Sequence (STS), it is already extended to 64 and scaled as
+% in 802.11a
+load('stsFreq.mat');
 
-shortTrainingSeq = [zeros(6,1); sqrt(13/6)*shortTrainingSeq; zeros(5,1)];
-shortTrainingSeqIFFT = ifft(fftshift(shortTrainingSeq), config.NFFT_TS/4);
+% Long Training Sequence (LTS), it is already extended to 64 and scaled as
+% in 802.11a
+load('ltsFreq.mat');
 
-longTrainingSeq = [zeros(6,1); longTrainingSeq; zeros(5,1)];
-longTrainingSeqIFFT = ifft(fftshift(longTrainingSeq), config.NFFT_TS);
+fourStsTime = ifft(fftshift(stsFreq), config.NFFT_TS);
+stsTime = fourStsTime(1:16);
+ltsTime = ifft(fftshift(ltsFreq), config.NFFT_TS);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Information symbols Generation
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-numSymbols = (config.numOFDMSymbolsPerFrame-1)*(config.numDataCarriers-config.numPilots); % -1 to account for the pilot symbol
-symbols = modulator(randi([0,config.M-1], numSymbols, 1), config.map);
 
-pilotOfdmSymbol = modulator(randi([0,config.M-1], config.numDataCarriers, 1), config.map);
+% Number of useful data symbol to send
+numDataSymbols = (config.numOFDMSymbolsPerFrame-1)*config.numDataCarriers
+dataSymbolsDec = randi([0,config.M-1], numDataSymbols, 1);
 
-pilots1 = ones(1, config.numOFDMSymbolsPerFrame-1);
-pilots2 = ones(1, config.numOFDMSymbolsPerFrame-1);
+% Get data symbols from constellation
+dataSymbols = modulator(dataSymbolsDec, config.map);
+
+% Pilot OFDM symbols (excluding pilot subcarriers) for channel estimation
+% Randomization is needed to avoid high PAPR
+% TODO: Use pseudorandomization to be able to use with USRPs on separate
+% hosts !
+% Note: To estimate the noise variance for MMSE equalization, it is needed
+% that re(X) = im(X) where X is one symbol of the pilot OFDM symbol.
+pilotOfdmSymbol = config.map(randi([0,1], config.numDataCarriers, 1)+2).';
+
+% Pilot subcarriers to correct for Sampling Frequency Offset (SFO) 
+% Note: the first symbol correspond to the pilot OFDM symbol
+pilotsSc1 = [config.map(1), repmat(config.map(config.M), 1, config.numOFDMSymbolsPerFrame-1)];
+pilotsSc2 = [config.map(4), repmat(config.map(config.M), 1, config.numOFDMSymbolsPerFrame-1)];
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Transmitter
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-[signalTx, dataFrame] = OFDMTransmitter(config, mode, symbols, shortTrainingSeqIFFT, longTrainingSeqIFFT, pilotOfdmSymbol, pilots1, pilots2);
+% Build the OFDM frame (preamble, pilots, data)
+[signalTx, dataFrame] = OFDMTransmitter(config, mode, dataSymbols, stsTime, ltsTime, pilotOfdmSymbol, pilotsSc1, pilotsSc2);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Channel
@@ -73,22 +100,35 @@ pilots2 = ones(1, config.numOFDMSymbolsPerFrame-1);
 
 figure;
 plot(abs(signalTx));
+xlabel('Samples');
+ylabel('Magnitude');
 title('Absolute value of transmitted signal');
 
-if strcmp(mode, 'simulation')
-    [signalRx, Fs] = simulatorOFDM(signalTx);
-else
-    signalTx = padWithZeros(signalTx, samplesPerFrame);
-    [signalRx,Fs] = rxTxUSRP(signalTx.', samplesPerFrame, mode); 
-    signalRx = signalRx.'; 
+% Transmission / Reception of the signal
+if strcmp(mode, 'simulation') % Use simulator
+    [signalRx, Fs] = simulatorOFDM(signalTx, config);
+else % Use USRPs
+    [signalRx,Fs] = rxTxUSRP(signalTx.', mode, config); 
+    signalRx = signalRx.';
+    signalRx = signalRx ./ max(abs(signalRx)); %TMP (?)
+    
+    % Truncate begining of the signal: contains garbage (how come ?)
     signalRx = signalRx(1e5:end);
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Receiver
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Signal equalization and decoding
 if strcmp(mode, 'simulation') || strcmp(mode, 'twoBoardsRxTx') || strcmp(mode, 'oneBoardRx') || strcmp(mode, 'loopback')
-    symbolsRx = OFDMReceiver(signalRx, config, shortTrainingSeqIFFT, longTrainingSeqIFFT, pilotOfdmSymbol, pilots1, pilots2, dataFrame);
+    dataSymbolsRx = OFDMReceiver(signalRx, config, stsTime, ltsTime, pilotOfdmSymbol, pilotsSc1, pilotsSc2, dataFrame);
+    
+    % Demodulates symbols estimate(Hard Decision)
+    dataSymbolsDecEst = demodulator(dataSymbolsRx, config.map);
+    
+    % Compute Symbol Error Rate (SER)
+    SER = sum(dataSymbolsDec ~= dataSymbolsDecEst)/length(dataSymbolsDec)
 end
 
 fprintf('Instance terminated on %s \n\n',datestr(now));
