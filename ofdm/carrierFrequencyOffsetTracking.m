60fbca8c2b9a0d0bcb23bb25bf092bc4a51feb66
Michael Heiniger <michael.heiniger@epfl.ch>
2017-06-06 10:43:23 +0200

    Correct error: reference pilots1 and pilots2 should not be completed with received OFDM pilot symbol but with clean OFDM pilot symbol. This is now externalized in OFDMReceiver

--- ofdm/carrierFrequencyOffsetTracking.m
+++ ofdm/carrierFrequencyOffsetTracking.m
@@ -1,13 +1,9 @@
-function [ofdmSymbolsCorrected] = carrierFrequencyOffsetTracking(ofdmSymbols, pilots1, pilots2)
+function [ofdmSymbolsCorrected] = carrierFrequencyOffsetTracking(ofdmSymbolsRx, pilots1, pilots2)
 %CARRIERFREQUENCYOFFSETTRACKING This function uses the pilot symbols
 %present in each OFDM symbol to track and remove the residual CFO
 
-pilots1Rx = ofdmSymbols(1,:);
-pilots2Rx = ofdmSymbols(end,:);
-
-pilotOfdmSymbolRx = ofdmSymbols(:,1);
-pilots1 = [pilotOfdmSymbolRx(1), pilots1];
-pilots2 = [pilotOfdmSymbolRx(end), pilots2];
+pilots1Rx = ofdmSymbolsRx(1,:);
+pilots2Rx = ofdmSymbolsRx(end,:);
 
 % Compute the phase of pilot symbols for every OFDM symbols
 phase1 = angle(pilots1Rx.*conj(pilots1));
@@ -17,7 +13,7 @@ phase2 = angle(pilots2Rx.*conj(pilots2));
 meanPhase = (phase1+phase2)/2;
 
 % Remove the phase from all OFDM symbols
-ofdmSymbolsCorrected = ofdmSymbols .* exp(-1j*meanPhase);
+ofdmSymbolsCorrected = ofdmSymbolsRx .* exp(-1j*meanPhase);
 
 end
 
