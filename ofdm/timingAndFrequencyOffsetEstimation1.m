ea22e3ce89105cc508c326e5a2fad869b9003baa
Michael Heiniger <michael.heiniger@epfl.ch>
2017-06-17 17:31:53 +0200

    Coarse CFO is now computed once and for all timing candidates. This is possible since we use STS8 and STS9 which are surrounded by STS7 and STS10 they are protected by ISI and are valid for any delay smaller than one length of STS which is equal to the length of the cyclic prefix or regular OFDM symbols which is assumed to be larger than the length of the channel impulse response. +

--- ofdm/timingAndFrequencyOffsetEstimation1.m
+++ ofdm/timingAndFrequencyOffsetEstimation1.m
@@ -1,4 +1,4 @@
-function [timingEst, frequencyOffsetEst, initialPhaseEst] = timingAndFrequencyOffsetEstimation1(systemConfig, signal, stsTime, ltsTime, ca)
+function [timingEst, frequencyOffsetEst, initialPhaseEst] = timingAndFrequencyOffsetEstimation1(systemConfig, receiverConfig, signal, stsTime, ltsTime)
 %TIMINGANDFREQUENCYOFFSETESTIMATION1 Find the beginning of the frame, the
 %frequency offset and intial phase.
 % Returned values are:
@@ -10,25 +10,13 @@ function [timingEst, frequencyOffsetEst, initialPhaseEst] = timingAndFrequencyOf
 disp('Timing and Frequency Offset Estimation using STS/LTS and OFDM demodulation')
 
 sc = systemConfig;
+rc = receiverConfig;
 
 Ts = 1/sc.Fs;
 
-% Coarse timing estimation
-% Cross-correlation between 10 clean shorts STS with the signal
-tenStsTime = repmat(stsTime,10,1); 
-[R, ~] = xcorr(signal, repmat(stsTime,10,1));
-R = R(length(signal):end);
-R = R / sum(abs(tenStsTime).^2); % Normalization
-[~, coarseTau] = max(abs(R));
-
-plotSignalMagnitude(R, 'Offset [samples]', 'Cross-correlation (signal, 10 STS)');
-
-% coarseTimingWindowLength = sc.CPLength+20; % maximum length of ISI 
-coarseTimingWindowLength = sc.CPLength; % maximum length of ISI 
-
-% Indices of samples candidate for first frame sample
-fineTimingWindow = (coarseTau-coarseTimingWindowLength:coarseTau);
-% fineTimingWindow = (coarseTau-coarseTimingWindowLength:coarseTau+coarseTimingWindowLength);
+% Indices of samples candidate as first sample of the OFDM frame
+% maximum length of ISI is smaller or equal than cyclic prefix length
+fineTimingWindow = (1:1+sc.CPLength);
 
 frameOffsetStartTwoLts = 10*length(stsTime) + sc.twoLtsCpLength;
 
@@ -36,24 +24,26 @@ allMSEs = zeros(1,length(fineTimingWindow));
 allMAEs = zeros(1,length(fineTimingWindow));
 
 % Store the CFO estimations for every tentative timing
-coarseCFO = zeros(1,length(fineTimingWindow));
+% coarseCFO = zeros(1,length(fineTimingWindow));
 fineCFO = zeros(1,length(fineTimingWindow));
 initialPhase = zeros(1,length(fineTimingWindow));
+
+% Coarse CFO estimation, valid for N_ISI smaller than length of STS (which
+% is equal to the cyclic prefix length which is assumed larger than N_ISI)
+stsTime8 = signal(fineTimingWindow(1)+7*length(stsTime):fineTimingWindow(1)+8*length(stsTime)-1);
+stsTime9 = signal(fineTimingWindow(1)+8*length(stsTime):fineTimingWindow(1)+9*length(stsTime)-1);
+coarseCFO = 1/(2*pi*length(stsTime)*Ts)*angle(doInnerProduct(stsTime9, stsTime8));
 
 % Loop over all timing candidates
 for d = 1:length(fineTimingWindow)
     ltsTimeRx1 = signal(fineTimingWindow(d)+frameOffsetStartTwoLts:fineTimingWindow(d)+frameOffsetStartTwoLts+length(ltsTime)-1); % LTS 1
     ltsTimeRx2 = signal(fineTimingWindow(d)+frameOffsetStartTwoLts+length(ltsTime):fineTimingWindow(d)+frameOffsetStartTwoLts+2*length(ltsTime)-1); % LTS 2
-        
-    stsTime8 = signal(fineTimingWindow(d)+7*length(stsTime):fineTimingWindow(d)+8*length(stsTime)-1);
-    stsTime9 = signal(fineTimingWindow(d)+8*length(stsTime):fineTimingWindow(d)+9*length(stsTime)-1);
     
-    % Coarse CFO estimation and correction
-    coarseCFO(d) = 1/(2*pi*length(stsTime)*Ts)*angle(doInnerProduct(stsTime9, stsTime8));
+    % Coarse CFO correction
     t1 = (0:length(ltsTimeRx1)-1)*Ts;
     t2 = (length(ltsTimeRx1):2*length(ltsTimeRx1)-1)*Ts;
-    ltsTimeRxCorrected1 = ltsTimeRx1 .* exp(-1j*2*pi*coarseCFO(d)*t1).';
-    ltsTimeRxCorrected2 = ltsTimeRx2 .* exp(-1j*2*pi*coarseCFO(d)*t2).';
+    ltsTimeRxCorrected1 = ltsTimeRx1 .* exp(-1j*2*pi*coarseCFO*t1).';
+    ltsTimeRxCorrected2 = ltsTimeRx2 .* exp(-1j*2*pi*coarseCFO*t2).';
     
     % Fine CFO estimation and correction
     fineCFO(d) = 1/(2*pi*length(ltsTime)*Ts)*angle(doInnerProduct(ltsTimeRxCorrected2, ltsTimeRxCorrected1));
@@ -95,49 +85,53 @@ for d = 1:length(fineTimingWindow)
     allMSEs(d) = mean(abs([ltsFreq(7:32);ltsFreq(34:59); ltsFreq(7:32);ltsFreq(34:59)]-[ltsFreqRxCorrected1(7:32);ltsFreqRxCorrected1(34:59);ltsFreqRxCorrected2(7:32);ltsFreqRxCorrected2(34:59)]).^2); % Mean Squared Error 
 %     allMSEs(d) = mean(abs(ltsFreq-ltsFreqRxCorrected2).^2); % Mean Squared Error 
 %     allMAEs(d) = mean(abs(ltsFreq-ltsFreqRxCorrected2)); % Mean Absolute Error
-    allMAEs(d) = mean(abs([ltsFreq(7:32);ltsFreq(34:59)]-[ltsFreqRxCorrected2(7:32);ltsFreqRxCorrected2(34:59)])); % Mean Absolute Error
+%     allMAEs(d) = mean(abs([ltsFreq(7:32);ltsFreq(34:59)]-[ltsFreqRxCorrected2(7:32);ltsFreqRxCorrected2(34:59)])); % Mean Absolute Error
 end
 
-threshold = 0.25;
-quantileError = quantile(allMSEs, [threshold, 1])
+quantileError = quantile(allMSEs, [rc.timingThreshold, 1])
 % Take the sample which has an error below the treshold and which has the highest index 
 posQuantile = find(allMSEs <= quantileError(1), 1, 'last');
 fineTauQuantile = fineTimingWindow(posQuantile)
 
-figure;
-hax=axes;
-stem(fineTimingWindow, abs(allMSEs),'.')
-HL1 = quantileError(1);
-line(get(hax,'XLim'),[HL1 HL1], 'Color', [1 0 0]);
-title('Total error on LTS vs offset');
-
 [minMSE, posMinMSE] = min(allMSEs);
-[minMAE, posMinMAE] = min(allMAEs);
+% [minMAE, posMinMAE] = min(allMAEs);
 % disp(['Minimum MSE: ', num2str(minMSE)]);
 % disp(['Minimum MAE: ', num2str(minMAE)]);
 
-fineTauMinMSE = fineTimingWindow(posMinMSE)
-fineTauMinMAE = fineTimingWindow(posMinMAE);
+fineTauMinMSE = fineTimingWindow(posMinMSE);
+% fineTauMinMAE = fineTimingWindow(posMinMAE);
 fineTau = fineTauQuantile;
 % fineTau = fineTimingWindow(posMinMSE)
 
+figure;
+hax=axes;
+stem(fineTimingWindow, allMSEs,'.')
+hold on;
+VL1 = fineTau;
+HL1 = quantileError(1);
+xlabel('Signal sample indices');
+ylabel('MSE');
+line([VL1 VL1], get(hax,'YLim'), 'Color', [0 1 0]);
+line(get(hax,'XLim'),[HL1 HL1], 'Color', [1 0 0]);
+title('MSE vs timing candidates');
+
 position = posQuantile;
-disp(['Coarse CFO: ', num2str(coarseCFO(position))]);
+disp(['Coarse CFO: ', num2str(coarseCFO)]);
 disp(['Fine CFO: ', num2str(fineCFO(position))]);
 
-% Begining of the frame: take into account the CA code
-frameBeginning = fineTau-length(ca);
+% Begining of the frame
+frameBeginning = fineTau;
 
 % Timing estimates is the estimate of the first sample of the frame
 timingEst = frameBeginning;
 
 % Total frequency offset is the sum of the coarse and fine estimations
-frequencyOffsetEst = coarseCFO(position) + fineCFO(position);
+frequencyOffsetEst = coarseCFO + fineCFO(position);
 
 % Initial phase offset wrt the beginning of the frame
 % Note: Initial phase offset is computed wrt LTS1 which is preceded by
-% CA, 10*STSs and the CP of LTS1 and LTS2
-offsetToFrameBeginning = length(ca)+length(tenStsTime)+sc.twoLtsCpLength;
+% 10*STSs and the CP of LTS1 and LTS2
+offsetToFrameBeginning = 10*length(stsTime)+sc.twoLtsCpLength;
 initialPhaseEst = angle(exp(1j*initialPhase(position)-1j*(2*pi*frequencyOffsetEst*offsetToFrameBeginning*Ts)));
 
 end
